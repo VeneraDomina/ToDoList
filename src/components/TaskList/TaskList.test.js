2f3fcbb9ae225c19d4610d5983ed4d8a67d3e702
Venera111 <Nelyapav@gmail.com>
2017-11-07 16:23:13 +0200

    test

--- src/components/TaskList/TaskList.test.js
+++ src/components/TaskList/TaskList.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { render } from 'enzyme';
+import { render, shallow } from 'enzyme';
 import TaskList from './';
+import TaskMaker from '../TaskMaker';
+import Task from '../Task';
 import { myProps } from '../../helpers/helperForTest';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 const { tasks } = myProps;
+const greeting = 'hello';
 const result = render(
     <TaskList
         createTask = { () => null }
@@ -20,7 +23,11 @@ describe('TaskList component:', () => {
     test('Should have 1 \'h1\' element', () => {
         expect(result.find('h1')).toHaveLength(1);
     });
-    test('Should have <\TaskMaker> component ', () => {
-        /*expect(result.find('section')).toHaveLength(1);*/
+    test('Should have 1 <\TaskMaker> component', () => {
+        expect(result.find(TaskMaker).length).toBe(1);
     });
+
+/*    test('Props tasks should be array ', () => {
+        expect(result.props.type).toEqual('Array');
+    });*/
 });
